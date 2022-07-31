e870cfd3400a4ebdb9d0740c3dbee6a29831c71c
undertayker <andrey.kozlovskiy99@mail.ru>
2022-07-31 23:22:29 +0300

    Create Program3.cs

---
+++ Program3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int allPictures = 52;
+            int picturesInRow = 3;
+            int falledRow = allPictures / picturesInRow;
+            allPictures -= falledRow * picturesInRow;
+
+            Console.WriteLine($"Заполняных рядов : {falledRow}");
+            Console.WriteLine($"Сверх меры : {allPictures}");
+        }
+    }
+} 
+    
