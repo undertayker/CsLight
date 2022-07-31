f763bdef54a50208f405ceece473740cc5e8490a
undertayker <andrey.kozlovskiy99@mail.ru>
2022-07-31 23:25:46 +0300

    Create Program4.cs

---
+++ Program4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Козловский";
+            string secondName = "Андрей";
+            Console.WriteLine("До : ");
+            Console.WriteLine(name);
+            Console.WriteLine(secondName);
+            Console.WriteLine();
+
+            string swapp;
+            swapp = name;
+            name = secondName;
+            secondName = swapp;
+            Console.WriteLine("После : ");
+            Console.WriteLine(name);
+            Console.WriteLine(secondName);
+        }
+    }
+} 
+    
