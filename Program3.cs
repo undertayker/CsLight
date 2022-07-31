036ae82879a3b62ad1a3b588b7bb065f81e83663
undertayker <andrey.kozlovskiy99@mail.ru>
2022-08-01 00:16:12 +0300

    Update Program3.cs

--- Program3.cs
+++ Program3.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 
-namespace Exercise 
+namespace Exercise1 
 {
-    class Program
+    class Program1
     {
         static void Main(string[] args)
         {
+
             int allPictures = 52;
+            int pictures;
+            pictures = allPictures;
             int picturesInRow = 3;
-            int falledRow = allPictures / picturesInRow;
-            allPictures -= falledRow * picturesInRow;
+            int falledRow = pictures / picturesInRow;
+            pictures -= falledRow * picturesInRow;
 
             Console.WriteLine($"Заполняных рядов : {falledRow}");
-            Console.WriteLine($"Сверх меры : {allPictures}");
+            Console.WriteLine($"Сверх меры : {pictures}");
         }
     }
 } 
