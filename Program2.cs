5000ba5a670b63dc0d46d01a63b5cf8e056874c8
undertayker <andrey.kozlovskiy99@mail.ru>
2022-07-31 23:13:26 +0300

    Update Program2.cs

--- Program2.cs
+++ Program2.cs
@@ -6,20 +6,20 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            string yourName;
+            string userName;
             string year;
             string zodiacSign;
             string work;
 
             Console.WriteLine("Введите ваше имя : ");
-            yourName = Console.ReadLine();
+            userName = Console.ReadLine();
             Console.WriteLine("Сколько вам лет : ");
             year = Console.ReadLine();
             Console.WriteLine("Ваш знак зодиака : ");
             zodiacSign = Console.ReadLine();
             Console.WriteLine("Где вы работаете : ");
             work = Console.ReadLine();
-            Console.WriteLine("Вас зовут : " + yourName);
+            Console.WriteLine("Вас зовут : " + userName);
             Console.WriteLine("Вам : " + year + "год");
             Console.WriteLine("Вы : " + zodiacSign);
             Console.WriteLine("Вы работаете : " + work);
