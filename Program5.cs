b6f5bc72cc77c736d61d66127719509371a3a3dc
undertayker <andrey.kozlovskiy99@mail.ru>
2022-08-01 00:50:50 +0300

    Create Program5.cs

---
+++ Program5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    class Program6
+    {
+        static void Main(string[] args)
+        {
+            int priceOfCrystal = 10;
+            Console.WriteLine("Добро пожаловать!");
+            Console.WriteLine($"Стоймость одного кристалла : {priceOfCrystal} " + " золотых");
+            Console.WriteLine("Введите ваше количество золота :");
+            int gold = int.Parse(Console.ReadLine());
+            Console.WriteLine("Сколько хотите купить кристалов?");
+            int numberOfCrystals = int.Parse(Console.ReadLine());
+
+            gold = priceOfCrystal % numberOfCrystals;
+            Console.WriteLine($"Осталось золота : {gold}");
+            Console.WriteLine($"куплено кристалов : {numberOfCrystals}");
+        } 
+    }
+}
