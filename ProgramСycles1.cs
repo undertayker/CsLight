8fa61ad7ee650532ecc6dd2ff609b6be5e4cd446
undertayker <andrey.kozlovskiy99@mail.ru>
2022-08-02 22:51:58 +0300

    Create ProgramСycles1.cs

---
+++ ProgramСycles1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+
+            string userMessage;
+            int userInput;
+            int numberOfRepetitions;
+
+            Console.WriteLine("Введите ваше сообщение: ");
+            userMessage = Console.ReadLine();
+            Console.WriteLine("Введите кол-во повторений вашего сообщения: ");
+            numberOfRepetitions = Convert.ToInt32(Console.ReadLine());
+
+            while (numberOfRepetitions > 0 )
+            {
+                Console.WriteLine(userMessage);
+                numberOfRepetitions--;               
+            }
+        }
+    }
+}
+
+
