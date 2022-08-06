37a3ab8ea0f66b764f9229dfd2fd2581bc64b40d
undertayker <andrey.kozlovskiy99@mail.ru>
2022-08-07 00:08:51 +0300

    Update ProgramСycles3.cs

--- ProgramСycles3.cs
+++ ProgramСycles3.cs
@@ -11,6 +11,21 @@ namespace CSLight
         static void Main(string[] args)
         {
             int number;
+            int a = 5;
+            int b = 12;
+            int c = 19;
+            int d = 26;
+            int e = 33;
+            int f = 40;
+            int g = 47;
+            int h = 54;              
+            int i = 61;
+            int j = 68;
+            int k = 75;
+            int l = 82;
+            int m = 89;  
+            int n = 96;
+
             for (number = 5; number < 100; number += 7)
             {
                 Console.WriteLine(number);
