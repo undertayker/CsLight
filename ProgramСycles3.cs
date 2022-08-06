135ce7cc138db964f92673b58f91271455f22748
undertayker <andrey.kozlovskiy99@mail.ru>
2022-08-06 23:25:03 +0300

    Create ProgramСycles3.cs

---
+++ ProgramСycles3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            for (number = 5; number < 100; number += 7)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
+
+
