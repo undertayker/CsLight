e9cd76519bd08371962d664c912431c84e2f7d93
undertayker <andrey.kozlovskiy99@mail.ru>
2022-08-07 04:08:26 +0300

    Create ProgramСycles4.cs

---
+++ ProgramСycles4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int number;
+            number = rand.Next(0, 100);
+            int amount = number;
+            Console.WriteLine("Числа кратные 3 или 5 : ");
+
+            for (int i = 0; i < number; i++)
+
+                if (i % 3 == 0 || i % 5 == 0)
+
+                {
+                    Console.WriteLine(i);
+                    amount += i;
+                }
+
+            Console.WriteLine("После суммы всех чисел мы получили : " + amount);
+        }
+    }
+}
+
+
