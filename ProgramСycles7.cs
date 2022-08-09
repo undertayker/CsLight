115bf8bb239a8fa344ecbba25983afad6857d2f2
undertayker <andrey.kozlovskiy99@mail.ru>
2022-08-09 18:38:36 +0300

    Create ProgramСycles7.cs

---
+++ ProgramСycles7.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            string characterInput = "";
+            string simbol;
+            int additionToLength = 2;
+
+            Console.WriteLine("Введите ваше имя : ");
+            userName = Console.ReadLine();
+            Console.WriteLine("Введите символ : ");
+            simbol = Console.ReadLine();
+            Console.WriteLine("Ваше имя : ");
+            Console.WriteLine(userName);
+            Console.WriteLine("Ваш символ : ");
+            Console.WriteLine(simbol);
+           
+            for (int i = 1; i <= (userName.Length + additionToLength + additionToLength); i += 1)
+            {
+                characterInput += simbol;
+            }
+            Console.WriteLine("Ваша картинка : ");
+            Console.WriteLine(characterInput);
+            Console.WriteLine(simbol + " " + userName + " " + simbol);
+            Console.WriteLine(characterInput);
+        }
+    }
+}
+
+
