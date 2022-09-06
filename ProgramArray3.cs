ff10b7a6a41c1e47cd20dc9d294750b44dd5a74d
undertayker <andrey.kozlovskiy99@mail.ru>
2022-09-06 09:30:34 +0300

    Update ProgramArray3.cs

--- ProgramArray3.cs
+++ ProgramArray3.cs
@@ -10,38 +10,52 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int[] array = new int[30];            
-            string localMaximum = " ";
-
-            for (int i = 0; i < array.GetLength(0); i++)
+            Random rand = new Random();
+            int[] array = new int[30];
+            
+            for (int i = 0; i < array.Length; i++)
             {
-                array[i] = random.Next(1, 9);
-                Console.WriteLine(array[i] + " ");
+                array[i] = rand.Next(1, 9);
+                Console.Write(array[i] + " ");
 
-                if ((i == array.GetLength(0) - 1) && (array[0] > array[1]))
+                if ((i + 1) % 10 == 0)
                 {
-                    localMaximum += array[0] + " ";
+                    Console.WriteLine();
                 }
+                    
+            }
 
-                if (i == array.GetLength(0) - 1)
+            Console.Write("Локальные максимумы: ");
+
+            int localMaxCount = 1;
+            bool repeatCheck = true;
+
+            if (array[0] > array[1])
+            {
+                Console.Write(localMaxCount++ + ")" + array[0] + " ");
+            }
+                
+            for (int i = 1; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i - 1] && array[i] > array[i + 1])
                 {
-                    for (int j = 1; j < array.GetLength(0) - 1; j++)
-                    {
-                        if (array[j] > array[j + 1] && array[j] > array[j - 1])
-                        {
-                            localMaximum += array[j] + " ";
-                        }
-                    }
+                    Console.Write(localMaxCount + ")" + array[i] + " ");
+                    localMaxCount++;
+                    repeatCheck = true;
                 }
-
-                if ((i == array.GetLength(0) - 1) && (array[array.GetLength(0) - 1] > array[array.GetLength(0) - 2]))
+                if ((localMaxCount - 1) % 10 == 0 && repeatCheck)
                 {
-                    localMaximum += " " + array[array.GetLength(0) - 1];
+                    Console.WriteLine();
+                    repeatCheck = false;
                 }
             }
 
-            Console.WriteLine($"Локальный максимум : {localMaximum}");
+            if (array[array.Length - 1] > array[array.Length - 2])
+            {
+                Console.Write(localMaxCount + ")" + array[array.Length - 1] + " ");
+            }
+
+            Console.WriteLine();
         }
     }
 }
