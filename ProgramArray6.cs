891d3b8265912a992451136b72d155a1be3d318f
undertayker <andrey.kozlovskiy99@mail.ru>
2022-09-14 13:47:06 +0300

    Update ProgramArray6.cs

--- ProgramArray6.cs
+++ ProgramArray6.cs
@@ -10,28 +10,27 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            int[] array = { 1, 4, 3, 8, 6, 5, 2, 7, 9, 10 };
+            int[] array = { 1, 4, 3, 8, 6, 5, 2, 7, 9, 10};
+            int temp = 0;
 
-            int sorting = 0;
-            
-            for(int i = 0; i < array.Length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
-
                 for (int j = i + 1; j < array.Length; j++)
                 {
                     if (array[i] > array[j])
                     {
-                        sorting = array[i];
+                        temp = array[i];
                         array[i] = array[j];
-                        array[j] = sorting;
+                        array[j] = array[i];
                     }
                 }
             }
+
             Console.WriteLine("Отсортированный массив : ");
 
             for (int i = 0; i < array.Length; i++)
             {
-                Console.WriteLine(array[i]);    
+                Console.WriteLine(array[i]);
             }
         }
     }
