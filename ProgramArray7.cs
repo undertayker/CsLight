d0c3dc968d5b908f975eadca4350bd9b13c6406d
undertayker <andrey.kozlovskiy99@mail.ru>
2022-09-21 18:21:19 +0300

    Update ProgramArray7.cs

--- ProgramArray7.cs
+++ ProgramArray7.cs
@@ -10,18 +10,13 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            string text = "Ехал Грека через реку видит Грека в реке рак ";
+            string text = "Ехал Грека через реку видит Грека в реке рак";
             string[] proposalConclusion = text.Split(' ');
 
-            Console.WriteLine(proposalConclusion[0]);
-            Console.WriteLine(proposalConclusion[1]);
-            Console.WriteLine(proposalConclusion[2]);
-            Console.WriteLine(proposalConclusion[3]);
-            Console.WriteLine(proposalConclusion[4]);
-            Console.WriteLine(proposalConclusion[5]);
-            Console.WriteLine(proposalConclusion[6]);
-            Console.WriteLine(proposalConclusion[7]);
-            Console.WriteLine(proposalConclusion[8]);
+            foreach (string i in proposalConclusion)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
