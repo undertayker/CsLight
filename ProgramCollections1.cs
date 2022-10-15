8e32710aa556fad15748e039fc9ed810fa6f186f
undertayker <andrey.kozlovskiy99@mail.ru>
2022-10-15 16:29:34 +0300

    Update ProgramCollections1.cs

--- ProgramCollections1.cs
+++ ProgramCollections1.cs
@@ -13,41 +13,25 @@ namespace Сollections
             string userInput;
 
             Dictionary<string, string> dictionary = new Dictionary<string, string>();
-            dictionary.Add("Компьютер", "это — устройство или система, способная автоматически выполнять заданную, изменяемую последовательность операций.");
-            dictionary.Add("Процессор", "это - Центральная часть компьютера, выполняющая заданные программой преобразования информации и осуществляющая управление всем вычислительным процессом.");
-            dictionary.Add("Оперативная память", "это - в большинстве случаев энергозависимая часть системы компьютерной памяти, в которой во время работы компьютера хранится выполняемый машинный код");
+            dictionary.Add("Компьютер", "Компьютер — это устройство или система, способная автоматически выполнять заданную, изменяемую последовательность операций.");
+            dictionary.Add("Процессор", "Процессор - это центральная часть компьютера, выполняющая заданные программой преобразования информации и осуществляющая управление всем вычислительным процессом.");
+            dictionary.Add("Оперативная память", "Оперативная память - это в большинстве случаев энергозависимая часть системы компьютерной памяти, в которой во время работы компьютера хранится выполняемый машинный код");
 
             Console.WriteLine("Добро пожаловать в мой небольшой словарь,в нем ты можешь узнать что такое :\nКомпьютер\nПроцессор\nОперативная память\nДля этого просто введи слово которое тебе интерестно :");
 
-            WordEntry(out userInput);
+            userInput = Console.ReadLine();
 
             Console.Clear();
 
             FindWord(dictionary, userInput);
         }
 
-        static void WordEntry(out string userInput)
+        static void FindWord(Dictionary<string, string>  dictionary, string userInput)
         {
-            userInput = Console.ReadLine();
-        }
-
-        static void FindWord(Dictionary<string, string> dictionary, string userInput)
-        {
-            bool isWork = false;
-
-            foreach (var word in dictionary)
+            if (dictionary.ContainsKey(userInput))
             {
-                if (userInput == word.Key)
-                {
-                    Console.WriteLine(dictionary[userInput]);
-                    isWork = true;
-                }
-            }
-
-            if (isWork == false)
-            {
-                Console.WriteLine("Данного слова в словаре нет!\nВсего хорошего, возвращайтесь к нам снова");
-            }
+                Console.WriteLine(dictionary[userInput]);
+            }          
         }
     }
 }
