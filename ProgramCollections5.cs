40cd961f981eb4e26268099ce8d51e12ff7591b9
undertayker <andrey.kozlovskiy99@mail.ru>
2022-11-23 19:06:53 +0300

    Create ProgramCollections5.cs

---
+++ ProgramCollections5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = { 1, 1, 1, 2, 3, 3, 3 };
+            int[] secondArray = { 2, 3, 3, 3, 4, 5, 5 };
+
+            List<int> numbers = new List<int>();
+
+            Write(numbers, firstArray.Length, firstArray);
+            Write(numbers, firstArray.Length, secondArray);
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        static void Write(List<int> numbers, int arrayLength, int[] array)
+        {
+            for (int i = 0; i < arrayLength; i++)
+            {
+                if (numbers.Contains(array[i]) == false)
+                {
+                    numbers.Add(array[i]);
+                }
+            }                       
+        }        
+    }
+}
